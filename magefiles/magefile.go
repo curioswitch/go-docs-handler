a8ec1505f781d15868ce7ecaf3d0f163f47d686f
Choko <choko@curioswitch.org>
2023-08-25 16:41:03 +0900

    Update docs client

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"fmt"
+	"io"
+	"io/fs"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
@@ -39,6 +46,65 @@ func Lint() error {
 // Check runs lint and tests.
 func Check() {
 	mg.SerialDeps(Lint, Test)
+}
+
+func FetchDocsClient() error {
+	resp, err := http.Get("https://repo1.maven.org/maven2/com/linecorp/armeria/armeria/1.25.1/armeria-1.25.1.jar")
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	zipReader, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
+	if err != nil {
+		return err
+	}
+
+	if err := fs.WalkDir(os.DirFS("docsclient"), ".", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+		switch path {
+		case "NOTICE.txt", "web-licenses.txt", ".":
+			return nil
+		}
+		return os.Remove(filepath.Join("docsclient", path))
+	}); err != nil {
+		return err
+	}
+
+	for _, f := range zipReader.File {
+		path := f.Name
+		if !strings.HasPrefix(path, "com/linecorp/armeria/server/docs/") || strings.HasSuffix(path, ".class") {
+			continue
+		}
+		if f.Name[len(f.Name)-1] == '/' {
+			// Directory
+			continue
+		}
+		path = path[len("com/linecorp/armeria/server/docs/"):]
+		// Windows compatibility
+		if path == "assets/favicon.png" {
+			path = filepath.Join("assets", "favicon.png")
+		}
+		file, err := f.Open()
+		if err != nil {
+			return err
+		}
+		content, err := io.ReadAll(file)
+		if err != nil {
+			return err
+		}
+		if err := os.WriteFile(filepath.Join("docsclient", path), content, 0o644); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func getProjects() []string {
